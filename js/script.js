eba99cdfb7840f2ea343aa57564cf8d3bf2409ea
Ksuzi <kseniaterenko@gmail.com>
2020-01-15 20:57:26 +0200

    правки

--- js/script.js
+++ js/script.js
@@ -53,4 +53,26 @@ $('a[href*="#"]')
    },
  
  });
-   }
+  }
+
+  $(function(){
+    $("#phone-f").mask("+3(099) 999-9999");
+  });
+
+  function validate(evt) {
+    var theEvent = evt || window.event;
+  
+    // Handle paste
+    if (theEvent.type === 'paste') {
+        key = event.clipboardData.getData('text/plain');
+    } else {
+    // Handle key press
+        var key = theEvent.keyCode || theEvent.which;
+        key = String.fromCharCode(key);
+    }
+    var regex = /[0-9]|\./;
+    if( !regex.test(key) ) {
+      theEvent.returnValue = false;
+      if(theEvent.preventDefault) theEvent.preventDefault();
+    }
+  }
